a0549ba9a163d08d1d535b98b53a21ca5b4d2317
Gustav Elsner <holobeat@gmail.com>
2019-11-22 23:07:48 -0500

    Add files via upload

---
+++ StdColor.elm
@@ -0,0 +1,1192 @@
+module StdColor exposing (..)
+
+{-| Color Names Supported by All Browsers
+All modern browsers support the following 140 color names.
+Visit https://www.w3schools.com/colors/colors_names.asp for color table.
+-}
+
+import Element exposing (Color, rgb255)
+
+
+{-| rgb(240, 248, 255)
+#F0F8FF
+-}
+aliceBlue : Color
+aliceBlue =
+    Element.rgb255 240 248 255
+
+
+{-| rgb(250, 235, 215)
+#FAEBD7
+-}
+antiqueWhite : Color
+antiqueWhite =
+    Element.rgb255 250 235 215
+
+
+{-| rgb(0, 255, 255)
+#00FFFF
+-}
+aqua : Color
+aqua =
+    Element.rgb255 0 255 255
+
+
+{-| rgb(127, 255, 212)
+#7FFFD4
+-}
+aquamarine : Color
+aquamarine =
+    Element.rgb255 127 255 212
+
+
+{-| rgb(240, 255, 255)
+#F0FFFF
+-}
+azure : Color
+azure =
+    Element.rgb255 240 255 255
+
+
+{-| rgb(245, 245, 220)
+#F5F5DC
+-}
+beige : Color
+beige =
+    Element.rgb255 245 245 220
+
+
+{-| rgb(255, 228, 196)
+#FFE4C4
+-}
+bisque : Color
+bisque =
+    Element.rgb255 255 228 196
+
+
+{-| rgb(0, 0, 0)
+#000000
+-}
+black : Color
+black =
+    Element.rgb255 0 0 0
+
+
+{-| rgb(255, 235, 205)
+#FFEBCD
+-}
+blanchedAlmond : Color
+blanchedAlmond =
+    Element.rgb255 255 235 205
+
+
+{-| rgb(0, 0, 255)
+#0000FF
+-}
+blue : Color
+blue =
+    Element.rgb255 0 0 255
+
+
+{-| rgb(138, 43, 226)
+#8A2BE2
+-}
+blueViolet : Color
+blueViolet =
+    Element.rgb255 138 43 226
+
+
+{-| rgb(165, 42, 42)
+#A52A2A
+-}
+brown : Color
+brown =
+    Element.rgb255 165 42 42
+
+
+{-| rgb(222, 184, 135)
+#DEB887
+-}
+burlyWood : Color
+burlyWood =
+    Element.rgb255 222 184 135
+
+
+{-| rgb(95, 158, 160)
+#5F9EA0
+-}
+cadetBlue : Color
+cadetBlue =
+    Element.rgb255 95 158 160
+
+
+{-| rgb(127, 255, 0)
+#7FFF00
+-}
+chartreuse : Color
+chartreuse =
+    Element.rgb255 127 255 0
+
+
+{-| rgb(210, 105, 30)
+#D2691E
+-}
+chocolate : Color
+chocolate =
+    Element.rgb255 210 105 30
+
+
+{-| rgb(255, 127, 80)
+#FF7F50
+-}
+coral : Color
+coral =
+    Element.rgb255 255 127 80
+
+
+{-| rgb(100, 149, 237)
+#6495ED
+-}
+cornflowerBlue : Color
+cornflowerBlue =
+    Element.rgb255 100 149 237
+
+
+{-| rgb(255, 248, 220)
+#FFF8DC
+-}
+cornsilk : Color
+cornsilk =
+    Element.rgb255 255 248 220
+
+
+{-| rgb(220, 20, 60)
+#DC143C
+-}
+crimson : Color
+crimson =
+    Element.rgb255 220 20 60
+
+
+{-| rgb(0, 255, 255)
+#00FFFF
+-}
+cyan : Color
+cyan =
+    Element.rgb255 0 255 255
+
+
+{-| rgb(0, 0, 139)
+#00008B
+-}
+darkBlue : Color
+darkBlue =
+    Element.rgb255 0 0 139
+
+
+{-| rgb(0, 139, 139)
+#008B8B
+-}
+darkCyan : Color
+darkCyan =
+    Element.rgb255 0 139 139
+
+
+{-| rgb(184, 134, 11)
+#B8860B
+-}
+darkGoldenRod : Color
+darkGoldenRod =
+    Element.rgb255 184 134 11
+
+
+{-| rgb(169, 169, 169)
+#A9A9A9
+-}
+darkGray : Color
+darkGray =
+    Element.rgb255 169 169 169
+
+
+{-| rgb(169, 169, 169)
+#A9A9A9
+-}
+darkGrey : Color
+darkGrey =
+    Element.rgb255 169 169 169
+
+
+{-| rgb(0, 100, 0)
+#006400
+-}
+darkGreen : Color
+darkGreen =
+    Element.rgb255 0 100 0
+
+
+{-| rgb(189, 183, 107)
+#BDB76B
+-}
+darkKhaki : Color
+darkKhaki =
+    Element.rgb255 189 183 107
+
+
+{-| rgb(139, 0, 139)
+#8B008B
+-}
+darkMagenta : Color
+darkMagenta =
+    Element.rgb255 139 0 139
+
+
+{-| rgb(85, 107, 47)
+#556B2F
+-}
+darkOliveGreen : Color
+darkOliveGreen =
+    Element.rgb255 85 107 47
+
+
+{-| rgb(255, 140, 0)
+#FF8C00
+-}
+darkOrange : Color
+darkOrange =
+    Element.rgb255 255 140 0
+
+
+{-| rgb(153, 50, 204)
+#9932CC
+-}
+darkOrchid : Color
+darkOrchid =
+    Element.rgb255 153 50 204
+
+
+{-| rgb(139, 0, 0)
+#8B0000
+-}
+darkRed : Color
+darkRed =
+    Element.rgb255 139 0 0
+
+
+{-| rgb(233, 150, 122)
+#E9967A
+-}
+darkSalmon : Color
+darkSalmon =
+    Element.rgb255 233 150 122
+
+
+{-| rgb(143, 188, 143)
+#8FBC8F
+-}
+darkSeaGreen : Color
+darkSeaGreen =
+    Element.rgb255 143 188 143
+
+
+{-| rgb(72, 61, 139)
+#483D8B
+-}
+darkSlateBlue : Color
+darkSlateBlue =
+    Element.rgb255 72 61 139
+
+
+{-| rgb(47, 79, 79)
+#2F4F4F
+-}
+darkSlateGray : Color
+darkSlateGray =
+    Element.rgb255 47 79 79
+
+
+{-| rgb(47, 79, 79)
+#2F4F4F
+-}
+darkSlateGrey : Color
+darkSlateGrey =
+    Element.rgb255 47 79 79
+
+
+{-| rgb(0, 206, 209)
+#00CED1
+-}
+darkTurquoise : Color
+darkTurquoise =
+    Element.rgb255 0 206 209
+
+
+{-| rgb(148, 0, 211)
+#9400D3
+-}
+darkViolet : Color
+darkViolet =
+    Element.rgb255 148 0 211
+
+
+{-| rgb(255, 20, 147)
+#FF1493
+-}
+deepPink : Color
+deepPink =
+    Element.rgb255 255 20 147
+
+
+{-| rgb(0, 191, 255)
+#00BFFF
+-}
+deepSkyBlue : Color
+deepSkyBlue =
+    Element.rgb255 0 191 255
+
+
+{-| rgb(105, 105, 105)
+#696969
+-}
+dimGray : Color
+dimGray =
+    Element.rgb255 105 105 105
+
+
+{-| rgb(105, 105, 105)
+#696969
+-}
+dimGrey : Color
+dimGrey =
+    Element.rgb255 105 105 105
+
+
+{-| rgb(30, 144, 255)
+#1E90FF
+-}
+dodgerBlue : Color
+dodgerBlue =
+    Element.rgb255 30 144 255
+
+
+{-| rgb(178, 34, 34)
+#B22222
+-}
+fireBrick : Color
+fireBrick =
+    Element.rgb255 178 34 34
+
+
+{-| rgb(255, 250, 240)
+#FFFAF0
+-}
+floralWhite : Color
+floralWhite =
+    Element.rgb255 255 250 240
+
+
+{-| rgb(34, 139, 34)
+#228B22
+-}
+forestGreen : Color
+forestGreen =
+    Element.rgb255 34 139 34
+
+
+{-| rgb(255, 0, 255)
+#FF00FF
+-}
+fuchsia : Color
+fuchsia =
+    Element.rgb255 255 0 255
+
+
+{-| rgb(220, 220, 220)
+#DCDCDC
+-}
+gainsboro : Color
+gainsboro =
+    Element.rgb255 220 220 220
+
+
+{-| rgb(248, 248, 255)
+#F8F8FF
+-}
+ghostWhite : Color
+ghostWhite =
+    Element.rgb255 248 248 255
+
+
+{-| rgb(255, 215, 0)
+#FFD700
+-}
+gold : Color
+gold =
+    Element.rgb255 255 215 0
+
+
+{-| rgb(218, 165, 32)
+#DAA520
+-}
+goldenRod : Color
+goldenRod =
+    Element.rgb255 218 165 32
+
+
+{-| rgb(128, 128, 128)
+#808080
+-}
+gray : Color
+gray =
+    Element.rgb255 128 128 128
+
+
+{-| rgb(128, 128, 128)
+#808080
+-}
+grey : Color
+grey =
+    Element.rgb255 128 128 128
+
+
+{-| rgb(0, 128, 0)
+#008000
+-}
+green : Color
+green =
+    Element.rgb255 0 128 0
+
+
+{-| rgb(173, 255, 47)
+#ADFF2F
+-}
+greenYellow : Color
+greenYellow =
+    Element.rgb255 173 255 47
+
+
+{-| rgb(240, 255, 240)
+#F0FFF0
+-}
+honeyDew : Color
+honeyDew =
+    Element.rgb255 240 255 240
+
+
+{-| rgb(255, 105, 180)
+#FF69B4
+-}
+hotPink : Color
+hotPink =
+    Element.rgb255 255 105 180
+
+
+{-| rgb(205, 92, 92)
+#CD5C5C
+-}
+indianRed : Color
+indianRed =
+    Element.rgb255 205 92 92
+
+
+{-| rgb(75, 0, 130)
+#4B0082
+-}
+indigo : Color
+indigo =
+    Element.rgb255 75 0 130
+
+
+{-| rgb(255, 255, 240)
+#FFFFF0
+-}
+ivory : Color
+ivory =
+    Element.rgb255 255 255 240
+
+
+{-| rgb(240, 230, 140)
+#F0E68C
+-}
+khaki : Color
+khaki =
+    Element.rgb255 240 230 140
+
+
+{-| rgb(230, 230, 250)
+#E6E6FA
+-}
+lavender : Color
+lavender =
+    Element.rgb255 230 230 250
+
+
+{-| rgb(255, 240, 245)
+#FFF0F5
+-}
+lavenderBlush : Color
+lavenderBlush =
+    Element.rgb255 255 240 245
+
+
+{-| rgb(124, 252, 0)
+#7CFC00
+-}
+lawnGreen : Color
+lawnGreen =
+    Element.rgb255 124 252 0
+
+
+{-| rgb(255, 250, 205)
+#FFFACD
+-}
+lemonChiffon : Color
+lemonChiffon =
+    Element.rgb255 255 250 205
+
+
+{-| rgb(173, 216, 230)
+#ADD8E6
+-}
+lightBlue : Color
+lightBlue =
+    Element.rgb255 173 216 230
+
+
+{-| rgb(240, 128, 128)
+#F08080
+-}
+lightCoral : Color
+lightCoral =
+    Element.rgb255 240 128 128
+
+
+{-| rgb(224, 255, 255)
+#E0FFFF
+-}
+lightCyan : Color
+lightCyan =
+    Element.rgb255 224 255 255
+
+
+{-| rgb(250, 250, 210)
+#FAFAD2
+-}
+lightGoldenRodYellow : Color
+lightGoldenRodYellow =
+    Element.rgb255 250 250 210
+
+
+{-| rgb(211, 211, 211)
+#D3D3D3
+-}
+lightGray : Color
+lightGray =
+    Element.rgb255 211 211 211
+
+
+{-| rgb(211, 211, 211)
+#D3D3D3
+-}
+lightGrey : Color
+lightGrey =
+    Element.rgb255 211 211 211
+
+
+{-| rgb(144, 238, 144)
+#90EE90
+-}
+lightGreen : Color
+lightGreen =
+    Element.rgb255 144 238 144
+
+
+{-| rgb(255, 182, 193)
+#FFB6C1
+-}
+lightPink : Color
+lightPink =
+    Element.rgb255 255 182 193
+
+
+{-| rgb(255, 160, 122)
+#FFA07A
+-}
+lightSalmon : Color
+lightSalmon =
+    Element.rgb255 255 160 122
+
+
+{-| rgb(32, 178, 170)
+#20B2AA
+-}
+lightSeaGreen : Color
+lightSeaGreen =
+    Element.rgb255 32 178 170
+
+
+{-| rgb(135, 206, 250)
+#87CEFA
+-}
+lightSkyBlue : Color
+lightSkyBlue =
+    Element.rgb255 135 206 250
+
+
+{-| rgb(119, 136, 153)
+#778899
+-}
+lightSlateGray : Color
+lightSlateGray =
+    Element.rgb255 119 136 153
+
+
+{-| rgb(119, 136, 153)
+#778899
+-}
+lightSlateGrey : Color
+lightSlateGrey =
+    Element.rgb255 119 136 153
+
+
+{-| rgb(176, 196, 222)
+#B0C4DE
+-}
+lightSteelBlue : Color
+lightSteelBlue =
+    Element.rgb255 176 196 222
+
+
+{-| rgb(255, 255, 224)
+#FFFFE0
+-}
+lightYellow : Color
+lightYellow =
+    Element.rgb255 255 255 224
+
+
+{-| rgb(0, 255, 0)
+#00FF00
+-}
+lime : Color
+lime =
+    Element.rgb255 0 255 0
+
+
+{-| rgb(50, 205, 50)
+#32CD32
+-}
+limeGreen : Color
+limeGreen =
+    Element.rgb255 50 205 50
+
+
+{-| rgb(250, 240, 230)
+#FAF0E6
+-}
+linen : Color
+linen =
+    Element.rgb255 250 240 230
+
+
+{-| rgb(255, 0, 255)
+#FF00FF
+-}
+magenta : Color
+magenta =
+    Element.rgb255 255 0 255
+
+
+{-| rgb(128, 0, 0)
+#800000
+-}
+maroon : Color
+maroon =
+    Element.rgb255 128 0 0
+
+
+{-| rgb(102, 205, 170)
+#66CDAA
+-}
+mediumAquaMarine : Color
+mediumAquaMarine =
+    Element.rgb255 102 205 170
+
+
+{-| rgb(0, 0, 205)
+#0000CD
+-}
+mediumBlue : Color
+mediumBlue =
+    Element.rgb255 0 0 205
+
+
+{-| rgb(186, 85, 211)
+#BA55D3
+-}
+mediumOrchid : Color
+mediumOrchid =
+    Element.rgb255 186 85 211
+
+
+{-| rgb(147, 112, 219)
+#9370DB
+-}
+mediumPurple : Color
+mediumPurple =
+    Element.rgb255 147 112 219
+
+
+{-| rgb(60, 179, 113)
+#3CB371
+-}
+mediumSeaGreen : Color
+mediumSeaGreen =
+    Element.rgb255 60 179 113
+
+
+{-| rgb(123, 104, 238)
+#7B68EE
+-}
+mediumSlateBlue : Color
+mediumSlateBlue =
+    Element.rgb255 123 104 238
+
+
+{-| rgb(0, 250, 154)
+#00FA9A
+-}
+mediumSpringGreen : Color
+mediumSpringGreen =
+    Element.rgb255 0 250 154
+
+
+{-| rgb(72, 209, 204)
+#48D1CC
+-}
+mediumTurquoise : Color
+mediumTurquoise =
+    Element.rgb255 72 209 204
+
+
+{-| rgb(199, 21, 133)
+#C71585
+-}
+mediumVioletRed : Color
+mediumVioletRed =
+    Element.rgb255 199 21 133
+
+
+{-| rgb(25, 25, 112)
+#191970
+-}
+midnightBlue : Color
+midnightBlue =
+    Element.rgb255 25 25 112
+
+
+{-| rgb(245, 255, 250)
+#F5FFFA
+-}
+mintCream : Color
+mintCream =
+    Element.rgb255 245 255 250
+
+
+{-| rgb(255, 228, 225)
+#FFE4E1
+-}
+mistyRose : Color
+mistyRose =
+    Element.rgb255 255 228 225
+
+
+{-| rgb(255, 228, 181)
+#FFE4B5
+-}
+moccasin : Color
+moccasin =
+    Element.rgb255 255 228 181
+
+
+{-| rgb(255, 222, 173)
+#FFDEAD
+-}
+navajoWhite : Color
+navajoWhite =
+    Element.rgb255 255 222 173
+
+
+{-| rgb(0, 0, 128)
+#000080
+-}
+navy : Color
+navy =
+    Element.rgb255 0 0 128
+
+
+{-| rgb(253, 245, 230)
+#FDF5E6
+-}
+oldLace : Color
+oldLace =
+    Element.rgb255 253 245 230
+
+
+{-| rgb(128, 128, 0)
+#808000
+-}
+olive : Color
+olive =
+    Element.rgb255 128 128 0
+
+
+{-| rgb(107, 142, 35)
+#6B8E23
+-}
+oliveDrab : Color
+oliveDrab =
+    Element.rgb255 107 142 35
+
+
+{-| rgb(255, 165, 0)
+#FFA500
+-}
+orange : Color
+orange =
+    Element.rgb255 255 165 0
+
+
+{-| rgb(255, 69, 0)
+#FF4500
+-}
+orangeRed : Color
+orangeRed =
+    Element.rgb255 255 69 0
+
+
+{-| rgb(218, 112, 214)
+#DA70D6
+-}
+orchid : Color
+orchid =
+    Element.rgb255 218 112 214
+
+
+{-| rgb(238, 232, 170)
+#EEE8AA
+-}
+paleGoldenRod : Color
+paleGoldenRod =
+    Element.rgb255 238 232 170
+
+
+{-| rgb(152, 251, 152)
+#98FB98
+-}
+paleGreen : Color
+paleGreen =
+    Element.rgb255 152 251 152
+
+
+{-| rgb(175, 238, 238)
+#AFEEEE
+-}
+paleTurquoise : Color
+paleTurquoise =
+    Element.rgb255 175 238 238
+
+
+{-| rgb(219, 112, 147)
+#DB7093
+-}
+paleVioletRed : Color
+paleVioletRed =
+    Element.rgb255 219 112 147
+
+
+{-| rgb(255, 239, 213)
+#FFEFD5
+-}
+papayaWhip : Color
+papayaWhip =
+    Element.rgb255 255 239 213
+
+
+{-| rgb(255, 218, 185)
+#FFDAB9
+-}
+peachPuff : Color
+peachPuff =
+    Element.rgb255 255 218 185
+
+
+{-| rgb(205, 133, 63)
+#CD853F
+-}
+peru : Color
+peru =
+    Element.rgb255 205 133 63
+
+
+{-| rgb(255, 192, 203)
+#FFC0CB
+-}
+pink : Color
+pink =
+    Element.rgb255 255 192 203
+
+
+{-| rgb(221, 160, 221)
+#DDA0DD
+-}
+plum : Color
+plum =
+    Element.rgb255 221 160 221
+
+
+{-| rgb(176, 224, 230)
+#B0E0E6
+-}
+powderBlue : Color
+powderBlue =
+    Element.rgb255 176 224 230
+
+
+{-| rgb(128, 0, 128)
+#800080
+-}
+purple : Color
+purple =
+    Element.rgb255 128 0 128
+
+
+{-| rgb(102, 51, 153)
+#663399
+-}
+rebeccaPurple : Color
+rebeccaPurple =
+    Element.rgb255 102 51 153
+
+
+{-| rgb(255, 0, 0)
+#FF0000
+-}
+red : Color
+red =
+    Element.rgb255 255 0 0
+
+
+{-| rgb(188, 143, 143)
+#BC8F8F
+-}
+rosyBrown : Color
+rosyBrown =
+    Element.rgb255 188 143 143
+
+
+{-| rgb(65, 105, 225)
+#4169E1
+-}
+royalBlue : Color
+royalBlue =
+    Element.rgb255 65 105 225
+
+
+{-| rgb(139, 69, 19)
+#8B4513
+-}
+saddleBrown : Color
+saddleBrown =
+    Element.rgb255 139 69 19
+
+
+{-| rgb(250, 128, 114)
+#FA8072
+-}
+salmon : Color
+salmon =
+    Element.rgb255 250 128 114
+
+
+{-| rgb(244, 164, 96)
+#F4A460
+-}
+sandyBrown : Color
+sandyBrown =
+    Element.rgb255 244 164 96
+
+
+{-| rgb(46, 139, 87)
+#2E8B57
+-}
+seaGreen : Color
+seaGreen =
+    Element.rgb255 46 139 87
+
+
+{-| rgb(255, 245, 238)
+#FFF5EE
+-}
+seaShell : Color
+seaShell =
+    Element.rgb255 255 245 238
+
+
+{-| rgb(160, 82, 45)
+#A0522D
+-}
+sienna : Color
+sienna =
+    Element.rgb255 160 82 45
+
+
+{-| rgb(192, 192, 192)
+#C0C0C0
+-}
+silver : Color
+silver =
+    Element.rgb255 192 192 192
+
+
+{-| rgb(135, 206, 235)
+#87CEEB
+-}
+skyBlue : Color
+skyBlue =
+    Element.rgb255 135 206 235
+
+
+{-| rgb(106, 90, 205)
+#6A5ACD
+-}
+slateBlue : Color
+slateBlue =
+    Element.rgb255 106 90 205
+
+
+{-| rgb(112, 128, 144)
+#708090
+-}
+slateGray : Color
+slateGray =
+    Element.rgb255 112 128 144
+
+
+{-| rgb(112, 128, 144)
+#708090
+-}
+slateGrey : Color
+slateGrey =
+    Element.rgb255 112 128 144
+
+
+{-| rgb(255, 250, 250)
+#FFFAFA
+-}
+snow : Color
+snow =
+    Element.rgb255 255 250 250
+
+
+{-| rgb(0, 255, 127)
+#00FF7F
+-}
+springGreen : Color
+springGreen =
+    Element.rgb255 0 255 127
+
+
+{-| rgb(70, 130, 180)
+#4682B4
+-}
+steelBlue : Color
+steelBlue =
+    Element.rgb255 70 130 180
+
+
+{-| rgb(210, 180, 140)
+#D2B48C
+-}
+tan : Color
+tan =
+    Element.rgb255 210 180 140
+
+
+{-| rgb(0, 128, 128)
+#008080
+-}
+teal : Color
+teal =
+    Element.rgb255 0 128 128
+
+
+{-| rgb(216, 191, 216)
+#D8BFD8
+-}
+thistle : Color
+thistle =
+    Element.rgb255 216 191 216
+
+
+{-| rgb(255, 99, 71)
+#FF6347
+-}
+tomato : Color
+tomato =
+    Element.rgb255 255 99 71
+
+
+{-| rgb(64, 224, 208)
+#40E0D0
+-}
+turquoise : Color
+turquoise =
+    Element.rgb255 64 224 208
+
+
+{-| rgb(238, 130, 238)
+#EE82EE
+-}
+violet : Color
+violet =
+    Element.rgb255 238 130 238
+
+
+{-| rgb(245, 222, 179)
+#F5DEB3
+-}
+wheat : Color
+wheat =
+    Element.rgb255 245 222 179
+
+
+{-| rgb(255, 255, 255)
+#FFFFFF
+-}
+white : Color
+white =
+    Element.rgb255 255 255 255
+
+
+{-| rgb(245, 245, 245)
+#F5F5F5
+-}
+whiteSmoke : Color
+whiteSmoke =
+    Element.rgb255 245 245 245
+
+
+{-| rgb(255, 255, 0)
+#FFFF00
+-}
+yellow : Color
+yellow =
+    Element.rgb255 255 255 0
+
+
+{-| rgb(154, 205, 50)
+#9ACD32
+-}
+yellowGreen : Color
+yellowGreen =
+    Element.rgb255 154 205 50
